1601c14995f86118bb4e6e83f57001670a1c6e2d
praydog <praydog@praydog.com>
2023-12-18 14:03:22 -0800

    Configs: Fix subdirectories not getting included (except plugins)

--- UnrealVR/GameConfig.cs
+++ UnrealVR/GameConfig.cs
@@ -15,19 +15,51 @@ namespace UnrealVR {
             try {
                 using (FileStream zipToOpen = new FileStream(destinationArchiveFileName, FileMode.Create))
                 using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create)) {
-                    foreach (var file in Directory.GetFiles(sourceDirectoryName)) {
-                        var entryName = Path.GetFileName(file);
-                        var entry = archive.CreateEntry(entryName);
-                        entry.LastWriteTime = File.GetLastWriteTime(file);
-                        using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                        using (var stream = entry.Open()) {
-                            fs.CopyTo(stream);
-                        }
-                    }
+                    DirectoryInfo di = new DirectoryInfo(sourceDirectoryName);
+                    string basePath = di.FullName;
+
+                    // Recursive method to process directories
+                    ProcessDirectory(di, basePath, archive);
                 }
             } catch (Exception ex) {
                 MessageBox.Show("An error occurred: " + ex.Message);
             }
+        }
+
+        private static void ProcessDirectory(DirectoryInfo di, string basePath, ZipArchive archive) {
+            // Process files in the directory
+            foreach (FileInfo file in di.GetFiles()) {
+                try {
+                    string entryName = GetRelativePath(file.FullName, basePath);
+                    var entry = archive.CreateEntry(entryName);
+                    entry.LastWriteTime = file.LastWriteTime;
+
+                    using (var fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (var stream = entry.Open()) {
+                        fs.CopyTo(stream);
+                    }
+                } catch(Exception) {
+                    continue;
+                }
+            }
+
+            // Recursively process subdirectories
+            foreach (DirectoryInfo subDi in di.GetDirectories()) {
+                if (subDi.Name.Equals("plugins", StringComparison.OrdinalIgnoreCase)) {
+                    continue;
+                }
+
+                ProcessDirectory(subDi, basePath, archive);
+            }
+        }
+
+        private static string GetRelativePath(string fullPath, string basePath) {
+            // Ensure trailing backslash
+            if (!basePath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal)) {
+                basePath += Path.DirectorySeparatorChar;
+            }
+
+            return fullPath.Substring(basePath.Length);
         }
 
         public static void ExtractZipToDirectory(string sourceArchiveFileName, string destinationDirectoryName) {
